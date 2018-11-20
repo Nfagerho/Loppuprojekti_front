5dd62d88ca8ae5c29fc809f8ba98e4a6cc1cc471
Nfagerho <Nooa.Fagerholm@wippies.fi>
2018-11-20 13:50:16 +0200

    Tehtiin frontin rakenne ja reitityksiä

--- src/index.js
+++ src/index.js
@@ -1,12 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import Etusivu from './komponentit/etusivu/Etusivu';
+import LoginSivu from './komponentit/login/LoginSivu';
+import { Switch, Route } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
+import SijainenLogin from './komponentit/login/sijainen/SijainenLogin';
+import KouluLogin from './komponentit/login/koulu/KouluLogin';
+import Rekisterointi from './komponentit/login/rekisterointi/Rekisterointi';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+ReactDOM.render((
+    <BrowserRouter>
+        <Switch>
+            <Route exact path='/' component={LoginSivu}></Route>
+            <Route exact path='/etusivu' component={Etusivu}></Route>
+            <Route exact path='/sijainenlogin' component={SijainenLogin}></Route>
+            <Route exact path='/koululogin' component={KouluLogin}></Route>
+            <Route exact path='/rekisterointi' component={Rekisterointi}></Route>
+            {/* <Route component={SivuaEiLoytynyt} /> */}
+        </Switch>
+    </BrowserRouter>
+), document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+

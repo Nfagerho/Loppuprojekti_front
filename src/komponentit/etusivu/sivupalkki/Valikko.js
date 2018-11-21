32462849bfd7bc7afd3e901396b16e7bf54aa9c0
Sami Eriksson <sami.eriksson@yahoo.fi>
2018-11-21 15:14:52 +0200

    Valikkoon lisätty komponentteja.

--- src/komponentit/etusivu/sivupalkki/Valikko.js
+++ src/komponentit/etusivu/sivupalkki/Valikko.js
@@ -1,5 +1,8 @@
 import React from "react";
 import Popup from 'reactjs-popup';
+import KaikkiToimeksiannot from "./sijainen/KaikkiToimeksiannot";
+import SijaisenToimeksiannot from "./sijainen/SijaisenToimeksiannot";
+import SijaisenTiedot from "./sijainen/SijaisenTiedot";
 
 // Tämä valikko tulee näkyviin, kun hampurilaista klikataan.
 export default ({ close }) => (
@@ -11,15 +14,20 @@ export default ({ close }) => (
     
     {/* Näissä alla olevissa linkeissä valikon sulkeminen (onClick={close}) ei toimi*/}
         <Popup trigger={<li onClick={close}>Omat sijaisuudet</li>} modal closeOnDocumentClick>
-            <span> Tähän lista omista sijaisuuksista. </span>
+            <span> Omat sijaisuudet: </span>
+            {/* Tähän täytyy laittaa ehtolause (jos lista tyhjä, mitä näytetään) */}
+            <SijaisenToimeksiannot/>
         </Popup>
 
         <Popup trigger={<li onClick={close}>Näytä kaikki vapaat sijaisuudet</li>} modal closeOnDocumentClick>
-            <span> Tähän kaikki vapaat sijaisuudet. </span>
+            <span> Kaikki vapaat sijaisuudet: </span>
+            {/* Tähän täytyy laittaa ehtolause (jos lista tyhjä, mitä näytetään) */}
+            <KaikkiToimeksiannot/>
         </Popup>
 
         <Popup trigger={<li onClick={close}>Omat tiedot</li>} modal closeOnDocumentClick>
             <span> Tähän sijaisen omat tiedot. </span>
+            <SijaisenTiedot/>
         </Popup>
     </ul>
   </div>

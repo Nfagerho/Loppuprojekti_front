aa469b63b543663e3606357a970acd5a1b06b8bb
akjr70150 <a.rossinen@gmail.com>
2018-11-22 17:03:04 +0200

    Laitettu nappuloita ja taustakuvaa

--- src/komponentit/login/LoginSivu.js
+++ src/komponentit/login/LoginSivu.js
@@ -1,18 +1,28 @@
-import React, { Component } from 'react';
-import { NavLink } from 'react-router-dom';
+import React, {Component} from 'react';
+import {NavLink} from 'react-router-dom';
 import SijainenLogin from './sijainen/SijainenLogin';
 import KouluLogin from './koulu/KouluLogin';
 import Rekisterointi from './rekisterointi/Rekisterointi';
-import { Switch, Route } from 'react-router-dom';
+import {Switch, Route} from 'react-router-dom';
+import './login.css';
+import Substidudes2 from './substidudes2.png'
+
+
+
+
 class LoginSivu extends Component {
+
     render() {
         return (
+
             <div>
-                LoginSivu
-                <nav>
-                    <NavLink to='/sijainenlogin'>SijainenLogin</NavLink><br/>
-                    <NavLink to='/koululogin'>KouluLogin</NavLink><br/>
-                    <NavLink to='/rekisterointi'>Rekisteröinti</NavLink>              
+            <div className="logo">
+                <img src={Substidudes2}/><br/><br/>
+            </div>
+                <nav className="login-menu">
+                    <NavLink className="button" to='/sijainenlogin'><span className="buttontext">SIJAINEN</span></NavLink><br/><br/>
+                    <NavLink className="button" to='/koululogin'><span className="buttontext2">KOULU</span></NavLink><br/><br/>
+                    <NavLink className="button" to='/rekisterointi'><span className="buttontext3">REKISTERÖIDY</span></NavLink><br/><br/>
                 </nav>
             </div>
         );

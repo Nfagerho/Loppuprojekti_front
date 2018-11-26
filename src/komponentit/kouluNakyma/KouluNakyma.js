e0f48fe2a44b8dc8b11ea66d8122c9f77347d17a
rzparvia <arno.p.parviainen@gmail.com>
2018-11-26 10:45:00 +0200

    Koulunäkymän tulostuksien asettelua

--- src/komponentit/kouluNakyma/KouluNakyma.js
+++ src/komponentit/kouluNakyma/KouluNakyma.js
@@ -1,11 +1,10 @@
 import React, {Component} from 'react';
 import Substidudes2 from '../substidudes2.png'
-import {Button, Col, ControlLabel, Form, FormControl, FormGroup, Grid, Row, Thumbnail} from "react-bootstrap";
+import {Button, Col, Grid, Row, Thumbnail} from "react-bootstrap";
 import koulu from './koulu.png';
 import './koulu.css';
-import KoulunToimeksiannot from "./KoulunToimeksiannot";
-import {haeKoulunTiedot} from "../../restpalvelu";
 import KoulunTiedot from "./koulu/KoulunTiedot";
+import KoulunToimeksiannot from "./koulu/KoulunToimeksiannot";
 
 class KouluNakyma extends Component {
 
@@ -17,16 +16,17 @@ class KouluNakyma extends Component {
                 </div>
                 <Grid>
                     <Row className="show-grid">
-                        <Col sm={4}>
-                            <Thumbnail src={koulu} alt="242x200">
+                        <Col sm={2}>
+                            <Thumbnail src={koulu}>
                                 <h3>Koulun nimi</h3>
                                 <p>Osoite</p>
                                 <p>
-                                    <Button bsStyle="primary">Vaihda kuva</Button>
+                                    <Button bsStyle="info">Vaihda kuva</Button>
                                 </p>
                             </Thumbnail>
                         </Col>
                         <Col sm={8}>
+                            <KoulunTiedot/>
                             <KoulunToimeksiannot/>
                         </Col>
                     </Row>

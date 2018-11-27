b779a025fac007216f10fbc980e00eb6b976335c
akjr70150 <a.rossinen@gmail.com>
2018-11-27 11:00:09 +0200

    säädetty sijaisen näkymään sisäänkirjautuneen tietoja

--- src/komponentit/sijainenNakyma/SijainenNakyma.js
+++ src/komponentit/sijainenNakyma/SijainenNakyma.js
@@ -4,7 +4,9 @@ import './valikko/sivupalkki.css';
 import Popup from 'reactjs-popup';
 import BurgerIkoni from './valikko/BurgerIkoni';
 import Valikko from './valikko/Valikko';
-import Substidudes2 from '../substidudes2.png'
+import Substidudes2 from '../substidudes2.png';
+import userSymbol from './userSymbol.png';
+
 
 
 // Autentikointiin liittyvää
@@ -32,6 +34,7 @@ class SijainenNakyma extends Component {
 
     constructor(props) {
         super(props);
+
         this.state = { sisaankirjautunut: '' };
     }
 
@@ -52,8 +55,8 @@ class SijainenNakyma extends Component {
 
                 <AuthUserContext.Consumer callbackfromparent = {this.callbackDataKomponentilta}>
                     {authUser => (
-                        <div>
-                            <p>Sisäänkirjautunut: {authUser.email}</p>
+                        <div className="sisaanKirjautunut">
+                            <p><img src={userSymbol} alt="userSymbol-logo" /> {authUser.email}</p>
                         </div>
                     )}
                 </AuthUserContext.Consumer>

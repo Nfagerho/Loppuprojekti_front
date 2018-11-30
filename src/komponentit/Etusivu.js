47313f73da2067a3ab89273d61659b556ef51749
Sami Eriksson <sami.eriksson@yahoo.fi>
2018-11-30 09:52:31 +0200

    Refactoroitu etusivua

--- src/komponentit/Etusivu.js
+++ src/komponentit/Etusivu.js
@@ -38,10 +38,11 @@ class Etusivu extends Component {
                     {/* <br/><br/><br/><br/><br/> */}
 
                         <div className="etusivukeskitettyDivi">
-                            <div className="etusivuvalkoinenDataboksi">
+                        {/* className="etusivuvalkoinenDataboksi" */}
+                            <div> 
                                 <div className="etusivukeskitettyData">
-                                    Substidudes sijaisopettajien välitykseen erikoistunut palvelu.
-                                    Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+                                    Substidudes on moderni ja helppokäyttöinen sijaisopettajien välityspalvelu. <br/><br/>  Uudet sijaisopettajat voivat rekisteröityä suoraan palvelun kautta. Koulut voivat ottaa palvelun käyttöön ottamalla yhteyttä hallintoon <a id="etusivunEmail" href="mailto:info.substidudes@gmail.com" target="_top">(info.substidudes ( at ) gmail.com).</a>
+                                    
                                 </div>
                             </div>
                         </div>
